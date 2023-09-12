fe5a0befcae9ec54d2028c963578fb6bd818b360
Wajud <wajudkareem757@gmail.com>
2023-09-12 21:53:20 +0100

    created the two functions

--- app.js
+++ app.js
@@ -1,3 +1,43 @@
-function handleSignin(){
 
+function handleSignIn(){
+
+    let localData = JSON.parse(localStorage.getItem("userInfo"));
+    const signinEmail = document.getElementById("signin-email").value;
+    const enteredPassword = document.getElementById("signin-password").value;
+
+    if(signinEmail != localData.signupEmail){
+        alert(`You do not have an account. Kindly SignUp instead`)
+    }
+
+    else {
+        if(enteredPassword != localData.userPassword){
+            alert(`You have entered an incorrect password`)
+        }
+
+        else{
+            alert(`Welcome Back ${localData.userName}`)
+        }
+    }
+
+
+}
+
+
+
+function handleSignUp(){
+
+    const userName = document.getElementById("username").value
+    const signupEmail = document.getElementById("signup-email").value;
+    const signupPassword = document.getElementById("signup-password").value;
+
+
+    let userInfo ={
+        userName: userName,
+        signupEmail: signupEmail,
+        userPassword: signupPassword
+    }
+
+    localStorage.setItem("userInfo", JSON.stringify(userInfo))
+
+   alert("That's done")
 }
